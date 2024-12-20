4e4fe4c63d2c43efa54fd9c42261cc6d57b33987
Alemskx <alemskx@gmail.com>
2024-12-21 00:39:54 +0300

    Task 6 complete

---
+++ collision/__init__.py
@@ -0,0 +1,5 @@
+from .CorrectRect import isCorrectRect
+from .CollisionRect import isCollisionRect
+from .intersectionAreaRect import intersectionAreaRect
+from .intersectionAreaMultiRect import intersectionAreaMultiRect
+
